1db62b2423e6fc96af502931d66b5802d9abd6c4
Manikandan <31544468+mani-arjunan@users.noreply.github.com>
2023-12-15 16:21:29 +0530

    Update keymaps.lua for live_grep search

--- .config/nvim/lua/mani/core/keymaps.lua
+++ .config/nvim/lua/mani/core/keymaps.lua
@@ -78,6 +78,7 @@ keymap.set("n", "<leader>fg", "<cmd>Telescope git_files<cr>") -- find string in 
 keymap.set("n", "<leader>fc", "<cmd>Telescope grep_string<cr>") -- find string under cursor in current working directory
 keymap.set("n", "<leader>fb", "<cmd>Telescope buffers<cr>") -- list open buffers in current neovim instance
 keymap.set("n", "<leader>fh", "<cmd>Telescope help_tags<cr>") -- list available help tags
+keymap.set("n", "<leader>fm", "<cmd>lua require('telescope').extensions.live_grep_args.live_grep_args()<CR>") -- live grep search 
 
 -- telescope git commands (not on youtube nvim video)
 keymap.set("n", "<leader>gc", "<cmd>Telescope git_commits<cr>") -- list all git commits (use <cr> to checkout) ["gc" for git commits]
