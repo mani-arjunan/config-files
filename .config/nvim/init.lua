3b78781c53c6126a550a45fab766cf513028c8fd
Manikandan <31544468+mani-arjunan@users.noreply.github.com>
2024-03-04 17:09:18 +0530

    Update init.lua

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -14,11 +14,12 @@ require("mani.plugins.lsp.null-ls")
 require("mani.plugins.autopairs")
 require("mani.plugins.treesitter")
 require("mani.plugins.gitsigns")
+require("mani.plugins.tabs")
+require("mani.plugins.indentlines")
+require("mani.plugins.formatter")
 require("mani.plugins.toggleterm")
 require("mani.core.set")
 require("mani.plugins.debugger")
-
-
 
 
 
