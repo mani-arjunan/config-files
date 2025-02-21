47cf6305862c00eee43e38fc21c45d27d27de234
Manikandan Arjunan <31544468+mani-arjunan@users.noreply.github.com>
2025-02-21 23:02:52 +0530

    Update keymaps.lua

--- .config/nvim/lua/mani/core/keymaps.lua
+++ .config/nvim/lua/mani/core/keymaps.lua
@@ -4,14 +4,8 @@ vim.g.mapleader = " "
 local keymap = vim.keymap -- for conciseness
 local opts = { noremap = true, silent = true }
 
----------------------
--- General Keymaps
----------------------
-
 -- use jk to exit insert mode
 keymap.set("i", "jk", "<ESC>")
-
-keymap.set("n", "cq", "cb")
 
 -- move any lines when selected
 keymap.set("v", "J", ":m '>+1<CR>gv=gv")
@@ -32,7 +26,6 @@ keymap.set("n", "<leader>s", [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left
 -- keymap.set to navigate between files
 keymap.set("n", "<C-p>", ":bp <CR>")
 keymap.set("n", "<C-n>", ":bn <CR>")
--- keymap.set tmux
 
 -- keymap.set back word from b to q
 keymap.set("n", "q", "b")
@@ -52,15 +45,15 @@ keymap.set("n", "<leader>+", "<C-a>") -- increment
 keymap.set("n", "<leader>-", "<C-x>") -- decrement
 
 -- window management
-keymap.set("n", "<leader>sv", "<C-w>v")        -- split window vertically
-keymap.set("n", "<leader>sh", "<C-w>s")        -- split window horizontally
-keymap.set("n", "<leader>se", "<C-w>=")        -- make split windows equal width & height
-keymap.set("n", "<leader>sx", ":close<CR>")    -- close current split window
+keymap.set("n", "<leader>sv", "<C-w>v") -- split window vertically
+keymap.set("n", "<leader>sh", "<C-w>s") -- split window horizontally
+keymap.set("n", "<leader>se", "<C-w>=") -- make split windows equal width & height
+keymap.set("n", "<leader>sx", ":close<CR>") -- close current split window
 
-keymap.set("n", "<leader>to", ":tabnew<CR>")   -- open new tab
+keymap.set("n", "<leader>to", ":tabnew<CR>") -- open new tab
 keymap.set("n", "<leader>tx", ":tabclose<CR>") -- close current tab
-keymap.set("n", "<leader>tn", ":tabn<CR>")     --  go to next tab
-keymap.set("n", "<leader>tp", ":tabp<CR>")     --  go to previous tab
+keymap.set("n", "<leader>tn", ":tabn<CR>") --  go to next tab
+keymap.set("n", "<leader>tp", ":tabp<CR>") --  go to previous tab
 
 ----------------------
 -- Plugin Keybinds
@@ -79,14 +72,14 @@ keymap.set("n", "<leader>E", vim.diagnostic.open_float, { desc = "Show diagnosti
 
 -- telescope
 keymap.set("n", "<leader><leader>", "<cmd>Telescope find_files<cr>") -- find files within current working directory, respects .gitignore
-keymap.set("n", "<leader>fs", "<cmd>Telescope live_grep<cr>")        -- find string in current working directory as you type
+keymap.set("n", "<leader>fs", "<cmd>Telescope live_grep<cr>") -- find string in current working directory as you type
 keymap.set("n", "<leader>fd", "<cmd>Telescope dir live_grep<CR>", { noremap = true, silent = true })
 keymap.set("n", "<leader>pd", "<cmd>Telescope dir find_files<CR>", { noremap = true, silent = true })
-keymap.set("n", "<leader>fg", "<cmd>Telescope git_files<cr>")   -- find string in current working directory as you type
+keymap.set("n", "<leader>fg", "<cmd>Telescope git_files<cr>") -- find string in current working directory as you type
 keymap.set("n", "<leader>fc", "<cmd>Telescope grep_string<cr>") -- find string under cursor in current working directory
-keymap.set("n", "<leader>fb", "<cmd>Telescope buffers<cr>")     -- list open buffers in current neovim instance
+keymap.set("n", "<leader>fb", "<cmd>Telescope buffers<cr>") -- list open buffers in current neovim instance
 keymap.set("n", "<leader>sd", "<cmd>Telescope diagnostics<CR>")
-keymap.set("n", "<leader>fh", "<cmd>Telescope help_tags<cr>")   -- list available help tags
+keymap.set("n", "<leader>fh", "<cmd>Telescope help_tags<cr>") -- list available help tags
 keymap.set("n", "<leader>gr", "<Cmd>Telescope lsp_references<CR>")
 keymap.set("n", "gd", "<cmd>Telescope lsp_definitions<CR>")
 keymap.set("n", "gi", "<cmd>Telescope lsp_implementations<CR>")
@@ -94,12 +87,11 @@ keymap.set("n", "gi", "<cmd>Telescope lsp_implementations<CR>")
 --     require("telescope.builtin").lsp_definitions({ jump_type = "never" })
 -- end, opts)
 
-
 -- telescope git commands (not on youtube nvim video)
-keymap.set("n", "<leader>gc", "<cmd>Telescope git_commits<cr>")   -- list all git commits (use <cr> to checkout) ["gc" for git commits]
+keymap.set("n", "<leader>gc", "<cmd>Telescope git_commits<cr>") -- list all git commits (use <cr> to checkout) ["gc" for git commits]
 keymap.set("n", "<leader>gfc", "<cmd>Telescope git_bcommits<cr>") -- list git commits for current file/buffer (use <cr> to checkout) ["gfc" for git file commits]
-keymap.set("n", "<leader>gb", "<cmd>Telescope git_branches<cr>")  -- list git branches (use <cr> to checkout) ["gb" for git branch]
-keymap.set("n", "<leader>gs", "<cmd>Telescope git_status<cr>")    -- list current changes per file with diff preview ["gs" for git status]
+keymap.set("n", "<leader>gb", "<cmd>Telescope git_branches<cr>") -- list git branches (use <cr> to checkout) ["gb" for git branch]
+keymap.set("n", "<leader>gs", "<cmd>Telescope git_status<cr>") -- list current changes per file with diff preview ["gs" for git status]
 
 -- line travel keymap
 keymap.set("n", "`", "0")
@@ -141,6 +133,7 @@ keymap.set("n", "<leader>tt", "<Cmd>Trouble<CR>")
 keymap.set("n", "<C-n>", "<Cmd>cnext<CR>")
 keymap.set("n", "<C-p>", "<Cmd>cprev<CR>")
 
--- for fuzzyfinder 
+-- for fuzzyfinder
 keymap.set("n", "<C-f>", "<Cmd>:silent !tmux neww ~/fzf_open.sh<CR>")
 keymap.set("n", "<C-q>", "<Cmd>:silent !tmux neww ~/fzf_queries.sh<CR>")
+keymap.set("n", "<leader>k", "<Cmd>:silent !~/tmux-kill-session.sh<CR>")
