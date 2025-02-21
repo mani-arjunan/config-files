3ae4eb211b2ac0b619289cfdc2bd74e2c89f98df
Manikandan Arjunan <31544468+mani-arjunan@users.noreply.github.com>
2025-02-21 22:45:12 +0530

    Update fzf_queries.sh

--- fzf_queries.sh
+++ fzf_queries.sh
@@ -1,9 +1,7 @@
 #!/bin/bash
 
-while true; do
-    FILE=$(find ~/queries -mindepth 1 -maxdepth 1 -type d | awk -F/ '{print $NF}' | fzf)
+FILE=$(find ~/queries -mindepth 1 -maxdepth 1 -type d | awk -F/ '{print $NF}' | fzf)
 
-    if [[ -n "$FILE" ]]; then
-      tmux new-window "cd "~/queries/$FILE" && ./index.sh"
-    fi
-done
+if [[ -n "$FILE" ]]; then
+  tmux new-window "cd ~/queries && ./index.sh $FILE"
+fi
