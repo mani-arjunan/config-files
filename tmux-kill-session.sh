382aa867e89d01bcb076d9c3414f78a1d6fd9f5c
Manikandan Arjunan <31544468+mani-arjunan@users.noreply.github.com>
2025-02-21 23:34:48 +0530

    Update tmux-kill-session.sh

--- tmux-kill-session.sh
+++ tmux-kill-session.sh
@@ -1,8 +1,15 @@
 #!/usr/bin/env bash
 
-if [[ $(tmux list-sessions | wc -l) -gt 1 ]]; then
-  tmux switch-client -l && tmux kill-session
-else
-  tmux kill-server
-fi
+CURRENT_SESSION=$(tmux display-message -p '#S')
+FIRST_SESSION=$(tmux list-sessions -F "#{session_name}" | head -n 1)
+NEXT_SESSION=$(tmux list-sessions -F "#{session_name}" | tail -n 1)
 
+if [[ "$CURRENT_SESSION" = "$FIRST_SESSION" ]]; then
+  if [[ -n "$NEXT_SESSION" ]]; then
+    tmux switch-client -t "$NEXT_SESSION" \; kill-session -t "$CURRENT_SESSION"
+  else
+    tmux kill-session -t "$CURRENT_SESSION"
+  fi
+else
+  tmux switch-client -t "$FIRST_SESSION" \; kill-session -t "$CURRENT_SESSION"
+fi
