aca219fb36d4fe26436dd4ccb2e0545aa5a0951c
Manikandan Arjunan <31544468+mani-arjunan@users.noreply.github.com>
2025-02-22 12:02:04 +0530

    Update init.lua

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -5,9 +5,10 @@ require("mani.core.colorscheme")
 require("mani.plugins.comment")
 require("mani.plugins.nvim-tree")
 require("mani.plugins.lualine")
+require("mani.plugins.git-worktree")
 require("mani.plugins.telescope")
-require("mani.plugins.nvim-cmp")
 require("mani.plugins.lsp.mason")
+require("mani.plugins.nvim-cmp")
 require("mani.plugins.lsp.lspsaga")
 require("mani.plugins.lsp.lspconfig")
 require("mani.plugins.lsp.null-ls")
@@ -15,15 +16,8 @@ require("mani.plugins.autopairs")
 require("mani.plugins.treesitter")
 require("mani.plugins.gitsigns")
 require("mani.plugins.tabs")
-require("mani.plugins.indentlines")
+-- require("mani.plugins.indentlines")
 require("mani.plugins.toggleterm")
 require("mani.core.set")
 require("mani.plugins.fold")
 require("mani.plugins.transparent")
-
-
-
-
-
-
-
