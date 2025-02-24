62bdd0dd73eef64d6d44b09d08ca8a0f187dd68c
Manikandan Arjunan <31544468+mani-arjunan@users.noreply.github.com>
2025-02-24 16:30:14 +0530

    Update create-tmux.sh

--- create-tmux.sh
+++ create-tmux.sh
@@ -5,6 +5,7 @@ function create_tmux_server () {
   is_git_needed="${4:-true}"
   custom_script="$5"
   start_shell="exec zsh"
+  EXCLUDED_NAMES="todo playground"
 
   cd "$path" || { echo "Not a valid path: $path"; return 1; }
 
@@ -14,7 +15,7 @@ function create_tmux_server () {
     tmux new -s "$name" -n "$name" -d "nvim .;$start_shell"
   fi
 
-  if [ "$windows_needed" = "true" ]; then
+  if [ "$windows_needed" = "true" ] && ! echo "$EXCLUDED_NAMES" | grep -wq "$name"; then
     tmux new-window -t "$name:2" -n "$name-exec" -d
     if [ "$is_git_needed" = "true" ]; then
       tmux new-window -t "$name:3" -n "$name-git" -d "lazygit;$start_shell"
