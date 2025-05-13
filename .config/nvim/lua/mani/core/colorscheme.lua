e3a86bdeb7d3e5a7b8e4d65b30216adf69c64b69
Manikandan Arjunan <31544468+mani-arjunan@users.noreply.github.com>
2025-05-13 17:55:34 +0530

    Update colorscheme.lua

--- .config/nvim/lua/mani/core/colorscheme.lua
+++ .config/nvim/lua/mani/core/colorscheme.lua
@@ -1,7 +1,33 @@
--- set colorscheme to nightfly with protected call
--- in case it isn't installed
-local status, _ = pcall(vim.cmd, "colorscheme habamax")
-if not status then
-  print("Colorscheme not found!") -- print error if colorscheme not installed
-  return
-end
+-- local ok, tokyo = pcall(require, "tokyonight")
+-- if not ok then
+--   print("tokyonight not found!")
+--   return
+-- end
+--
+-- tokyo.setup({
+--   style = "storm",
+--   transparent = true,
+--   terminal_colors = true,
+--   styles = {
+--     comments = { italic = false },
+--     keywords = { italic = false },
+--     functions = {},
+--     variables = {},
+--   },
+-- })
+
+vim.cmd.colorscheme("gruvbox-material")
+
+vim.cmd [[
+  highlight Normal guibg=NONE ctermbg=NONE
+  highlight NormalNC guibg=NONE ctermbg=NONE
+  highlight SignColumn guibg=NONE ctermbg=NONE
+  highlight MsgArea guibg=NONE ctermbg=NONE
+  highlight VertSplit guibg=NONE ctermbg=NONE
+]]
+
+vim.cmd [[
+  highlight NvimTreeNormal guibg=NONE ctermbg=NONE
+  highlight NvimTreeNormalNC guibg=NONE ctermbg=NONE
+  highlight NvimTreeEndOfBuffer guibg=NONE ctermbg=NONE
+]]
