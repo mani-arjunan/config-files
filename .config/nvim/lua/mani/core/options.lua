bcfebfad2cb1689c8d338be17a8a26c58941bbfc
Manikandan Arjunan <31544468+mani-arjunan@users.noreply.github.com>
2025-05-13 17:56:04 +0530

    Update options.lua

--- .config/nvim/lua/mani/core/options.lua
+++ .config/nvim/lua/mani/core/options.lua
@@ -1,26 +1,8 @@
 local opt = vim.opt -- for conciseness
 
--- line numbers
-opt.relativenumber = true -- show relative line numbers
-opt.number = true -- shows absolute line number on cursor line (when relative number is on)
-
--- tabs & indentation
-opt.tabstop = 2 -- 2 spaces for tabs (prettier default)
-opt.shiftwidth = 2 -- 2 spaces for indent width
-opt.expandtab = true -- expand tab to spaces
-opt.autoindent = true -- copy indent from current line when starting new one
-
--- line wrapping
-opt.wrap = false -- disable line wrapping
-
--- search settings
+-- -- search settings
 opt.ignorecase = true -- ignore case when searching
 opt.smartcase = true -- if you include mixed case in your search, assumes you want case-sensitive
-
--- cursor line
-opt.cursorline = true -- highlight the current cursor line
-
--- appearance
 
 -- turn on termguicolors for nightfly colorscheme to work
 -- (have to use iterm2 or any other true color terminal)
@@ -39,3 +21,13 @@ opt.splitright = true -- split vertical window to the right
 opt.splitbelow = true -- split horizontal window to the bottom
 
 opt.iskeyword:append("-") -- consider string-string as whole word
+
+opt.encoding = "utf-8"
+opt.expandtab = true
+
+opt.tabstop = 2
+opt.shiftwidth = 2
+opt.softtabstop = 2
+
+
+opt.fileformat = 'unix'
