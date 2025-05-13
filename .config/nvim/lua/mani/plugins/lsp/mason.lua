9c775f14e7ed6db5bf85a4100259b6304222fa95
Manikandan Arjunan <31544468+mani-arjunan@users.noreply.github.com>
2025-05-13 17:57:54 +0530

    Update mason.lua

--- .config/nvim/lua/mani/plugins/lsp/mason.lua
+++ .config/nvim/lua/mani/plugins/lsp/mason.lua
@@ -5,6 +5,9 @@ local mason_lspconfig = require("mason-lspconfig")
 local mason_tool_installer = require("mason-tool-installer")
 
 -- enable mason and configure icons
+-- with recent mason update, we don't need to install/configure any lsp, mason automatically detects
+-- lsp based on language at run time.
+-- But still keeping the old code commented for worst case.
 mason.setup()
 -- {
 --   ui = {
