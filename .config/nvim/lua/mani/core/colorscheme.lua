b210e63848163bdbac6e41b7be1211285dd8de88
Manikandan Arjunan <31544468+mani-arjunan@users.noreply.github.com>
2025-05-13 18:05:03 +0530

    Update colorscheme.lua

--- .config/nvim/lua/mani/core/colorscheme.lua
+++ .config/nvim/lua/mani/core/colorscheme.lua
@@ -1,23 +1,6 @@
--- local ok, tokyo = pcall(require, "tokyonight")
--- if not ok then
---   print("tokyonight not found!")
---   return
--- end
---
--- tokyo.setup({
---   style = "storm",
---   transparent = true,
---   terminal_colors = true,
---   styles = {
---     comments = { italic = false },
---     keywords = { italic = false },
---     functions = {},
---     variables = {},
---   },
--- })
-
 vim.cmd.colorscheme("gruvbox-material")
 
+-- for transparency
 vim.cmd [[
   highlight Normal guibg=NONE ctermbg=NONE
   highlight NormalNC guibg=NONE ctermbg=NONE
@@ -26,8 +9,10 @@ vim.cmd [[
   highlight VertSplit guibg=NONE ctermbg=NONE
 ]]
 
+-- to set transparency in nvim-tree
 vim.cmd [[
   highlight NvimTreeNormal guibg=NONE ctermbg=NONE
   highlight NvimTreeNormalNC guibg=NONE ctermbg=NONE
   highlight NvimTreeEndOfBuffer guibg=NONE ctermbg=NONE
+  highlight NvimTreeCursorLine guibg=#3c3836
 ]]
