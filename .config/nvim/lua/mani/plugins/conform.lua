de2c2dc50cee7204e75d00f5425ffd312008492e
Manikandan Arjunan <31544468+mani-arjunan@users.noreply.github.com>
2025-09-01 17:17:43 +0530

    Create conform.lua

---
+++ .config/nvim/lua/mani/plugins/conform.lua
@@ -0,0 +1,10 @@
+require("conform").setup({
+  formatters_by_ft = {
+    lua = { "stylua" },
+    go = { "gofmt" },
+    javascript = { "eslint_d" },
+    typescript = { "eslint_d" },
+    elixir = { "mix" },
+  },
+})
+
