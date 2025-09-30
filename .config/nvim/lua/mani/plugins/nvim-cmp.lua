3ab070e7e0d6073a460136b77b1e4602076dc452
Manikandan Arjunan <31544468+mani-arjunan@users.noreply.github.com>
2025-09-30 22:02:45 +0530

    Update nvim-cmp.lua

--- .config/nvim/lua/mani/plugins/nvim-cmp.lua
+++ .config/nvim/lua/mani/plugins/nvim-cmp.lua
@@ -16,9 +16,6 @@ if not lspkind_status then
   return
 end
 
--- load vs-code like snippets from plugins (e.g. friendly-snippets)
-require("luasnip/loaders/from_vscode").lazy_load()
-
 vim.opt.completeopt = "menu,menuone,noselect"
 
 cmp.setup({
@@ -43,11 +40,4 @@ cmp.setup({
     { name = "buffer" }, -- text within current buffer
     { name = "path" }, -- file system paths
   }),
-  -- configure lspkind for vs-code like icons
-  formatting = {
-    format = lspkind.cmp_format({
-      maxwidth = 50,
-      ellipsis_char = "...",
-    }),
-  },
 })
