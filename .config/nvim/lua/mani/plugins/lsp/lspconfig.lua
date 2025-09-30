4a7741f18fdb2f8abde6ab3e6b0766e398331c2c
Manikandan Arjunan <31544468+mani-arjunan@users.noreply.github.com>
2025-09-30 22:00:57 +0530

    Update lspconfig.lua

--- .config/nvim/lua/mani/plugins/lsp/lspconfig.lua
+++ .config/nvim/lua/mani/plugins/lsp/lspconfig.lua
@@ -10,10 +10,9 @@ if not cmp_nvim_lsp_status then
   return
 end
 
-
 local keymap = vim.keymap -- for conciseness
 
--- enable keybinds only for when lsp server available
+-- enable keybinds only for when lsp server is available
 local on_attach = function(client, bufnr)
   -- keybind options
   local opts = { noremap = true, silent = true, buffer = bufnr }
@@ -21,95 +20,80 @@ local on_attach = function(client, bufnr)
   -- set keybinds
   keymap.set("n", "<leader>ca", "<cmd>Lspsaga code_action<CR>", opts) -- see available code actions
   keymap.set("n", "<leader>rn", "<cmd>Lspsaga rename<CR>", opts) -- smart rename
-  keymap.set("n", "<leader>D", "<cmd>Lspsaga show_line_diagnostics<CR>", opts) -- show  diagnostics for line
+  keymap.set("n", "<leader>D", "<cmd>Lspsaga show_line_diagnostics<CR>", opts) -- show diagnostics for line
   keymap.set("n", "<leader>d", "<cmd>Lspsaga show_cursor_diagnostics<CR>", opts) -- show diagnostics for cursor
-  keymap.set("n", "K", "<cmd>Lspsaga hover_doc<CR>", opts) -- show documentation for what is under cursor
-  keymap.set("n", "<leader>o", "<cmd>LSoutlineToggle<CR>", opts) -- see outline on right hand side
+  keymap.set("n", "K", "<cmd>Lspsaga hover_doc<CR>", opts) -- show documentation for symbol under cursor
+  keymap.set("n", "<leader>o", "<cmd>LSoutlineToggle<CR>", opts) -- see outline on right-hand side
 
   -- typescript specific keymaps (e.g. rename file and update imports)
-  if client.name == "tsserver" then
+  if client.name == "tsserver" or client.name == "ts_ls" then
     keymap.set("n", "<leader>rf", ":TypescriptRenameFile<CR>") -- rename file and update imports
-    keymap.set("n", "<leader>oi", ":TypescriptOrganizeImports<CR>") -- organize imports (not in youtube nvim video)
-    keymap.set("n", "<leader>ru", ":TypescriptRemoveUnused<CR>") -- remove unused variables (not in youtube nvim video)
+    keymap.set("n", "<leader>oi", ":TypescriptOrganizeImports<CR>") -- organize imports
+    keymap.set("n", "<leader>ru", ":TypescriptRemoveUnused<CR>") -- remove unused variables
   end
 end
 
 -- used to enable autocompletion (assign to every lsp server config)
 local capabilities = cmp_nvim_lsp.default_capabilities()
 
--- Change the Diagnostic symbols in the sign column (gutter)
--- (not in youtube nvim video)
+-- Change the diagnostic symbols in the sign column (gutter)
 local signs = { Error = " ", Warn = " ", Hint = "ﴞ ", Info = " " }
 for type, icon in pairs(signs) do
   local hl = "DiagnosticSign" .. type
   vim.fn.sign_define(hl, { text = icon, texthl = hl, numhl = "" })
 end
 
--- configure html server
+-- configure servers
 lspconfig["html"].setup({
   capabilities = capabilities,
   on_attach = on_attach,
 })
 
--- configure typescript server with plugin
 lspconfig["ts_ls"].setup({
-  server = {
-    capabilities = capabilities,
-    on_attach = on_attach,
-  },
+  capabilities = capabilities,
+  on_attach = on_attach,
 })
 
--- configure typescript server with plugin
 lspconfig["gopls"].setup({
-  server = {
-    capabilities = capabilities,
-    on_attach = on_attach,
-  },
+  capabilities = capabilities,
+  on_attach = on_attach,
 })
 
--- configure angular language server
 lspconfig["angularls"].setup({
   capabilities = capabilities,
   on_attach = on_attach,
 })
 
--- configure css server
 lspconfig["cssls"].setup({
   capabilities = capabilities,
   on_attach = on_attach,
 })
 
--- configure tailwindcss server
 lspconfig["tailwindcss"].setup({
   capabilities = capabilities,
   on_attach = on_attach,
 })
 
--- configure emmet language server
 lspconfig["emmet_ls"].setup({
   capabilities = capabilities,
   on_attach = on_attach,
   filetypes = { "html", "typescriptreact", "javascriptreact", "css", "sass", "scss", "less", "svelte" },
 })
 
--- configure php language server
 lspconfig["intelephense"].setup({
   capabilities = capabilities,
   on_attach = on_attach,
 })
 
--- configure lua server (with special settings)
 lspconfig["lua_ls"].setup({
   capabilities = capabilities,
   on_attach = on_attach,
-  settings = { -- custom settings for lua
+  settings = {
     Lua = {
-      -- make the language server recognize "vim" global
       diagnostics = {
-        globals = { "vim" },
+        globals = { "vim" }, -- recognize `vim` global
       },
       workspace = {
-        -- make language server aware of runtime files
         library = {
           [vim.fn.expand("$VIMRUNTIME/lua")] = true,
           [vim.fn.stdpath("config") .. "/lua"] = true,
