cae7a8e1a5089bd83dc71c06b913d6ffb7a590b6
Manikandan Arjunan <31544468+mani-arjunan@users.noreply.github.com>
2025-11-24 17:38:37 +0530

    add lsp.lua init file

---
+++ .config/nvim/lua/mani/lsp.lua
@@ -0,0 +1,10 @@
+-- List of lsp's that i have
+vim.lsp.enable({"lua_ls"})
+vim.lsp.enable({"ts_ls"})
+vim.lsp.enable({"gopls"})
+vim.lsp.enable({"rust_analyzer"})
+vim.lsp.enable({"html"})
+vim.lsp.enable({"graphql"})
+vim.lsp.enable({"csharp_ls"})
+vim.lsp.enable({"css_ls"})
+
