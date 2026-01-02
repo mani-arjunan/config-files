5273601a7ce47d2449b642d076cb2f997f30211f
Manikandan Arjunan <31544468+mani-arjunan@users.noreply.github.com>
2026-01-02 21:17:13 +0530

    Update set.lua

--- .config/nvim/lua/mani/core/set.lua
+++ .config/nvim/lua/mani/core/set.lua
@@ -15,12 +15,14 @@ vim.opt.backup = false
 vim.opt.undodir = os.getenv("HOME") .. "/.vim/undodir"
 vim.opt.undofile = true
 
+
 vim.opt.hlsearch = true
 vim.opt.incsearch = true
 
 vim.opt.termguicolors = true
 
-vim.opt.scrolloff = 30
+vim.opt.scrolloff = 999
+-- vim.opt.virtualedit = "all"
 vim.opt.signcolumn = "yes"
 vim.opt.isfname:append("@-@")
 
@@ -35,5 +37,22 @@ vim.g.gruvbox_transparent_mode = 1
 
 -- hide fold related numbers
 vim.opt.foldcolumn = "0"
-vim.opt.fillchars:append({ eob = " " })
 vim.opt.laststatus = 0
+vim.opt.fillchars:append({ eob = " " })
+
+local function get_startup_root()
+  local argv = vim.fn.argv()
+
+  -- if nvim was started with a path
+  if #argv > 0 then
+    local path = vim.fn.fnamemodify(argv[1], ":p")
+    if vim.fn.isdirectory(path) == 1 then
+      return path
+    end
+  end
+
+  -- fallback to cwd
+  return vim.fn.getcwd(-1, -1)
+end
+
+vim.g.nvim_startup_root = get_startup_root()
