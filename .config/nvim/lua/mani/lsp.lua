f47dc98b25ec04faacbfa503eb230b0190a3e5a9
mani-arjunan <ronaldomanicr7@gmail.com>
2026-02-08 02:07:13 +0700

    change in lsp order

--- .config/nvim/lua/mani/lsp.lua
+++ .config/nvim/lua/mani/lsp.lua
@@ -1,12 +1,12 @@
 -- List of lsp's that i have
 vim.lsp.enable({
-  "lua_ls",
   "ts_ls",
-  "gopls",
-  "rust_analyzer",
   "html",
   "graphql",
-  "csharp_ls",
-  "css_ls",
-  "clangd"
+  "rust_analyzer",
+  -- "lua_ls",
+  -- "gopls",
+  -- "csharp_ls",
+  -- "css_ls",
+  -- "clangd"
 })
