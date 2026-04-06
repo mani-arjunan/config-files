4e31b834bf330d228f1982e7346363d4577119a5
Manikandan Arjunan <31544468+mani-arjunan@users.noreply.github.com>
2026-04-06 18:44:23 +0530

    Update conform.lua

--- .config/nvim/lua/mani/plugins/conform.lua
+++ .config/nvim/lua/mani/plugins/conform.lua
@@ -8,5 +8,21 @@ require("conform").setup({
     rust = { "rustfmt" },
     python = { "pylsp" }
   },
+  formatters = {
+    eslint_d = {
+      command = "eslint_d",
+      -- Override cwd to find config in monorepos / nested projects
+      cwd = require("conform.util").root_file({
+        "eslint.config.mjs",
+        "eslint.config.js",
+        ".eslintrc.js",
+        ".eslintrc.json",
+        "package.json",
+      }),
+    },
+  },
+  default_format_opts = {
+    timeout_ms = 5000,
+  },
 })
 
