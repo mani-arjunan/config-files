80fb096062db4b8a48e9b8c1090223574c897152
Manikandan Arjunan <31544468+mani-arjunan@users.noreply.github.com>
2026-09-15 21:49:06 +0530

    Update gopls.lua

--- .config/nvim/lsp/gopls.lua
+++ .config/nvim/lsp/gopls.lua
@@ -88,6 +88,11 @@ end
 ---@type vim.lsp.Config
 return {
   cmd = { 'gopls' },
+  settings = {
+    gopls = {
+      semanticTokens = true,
+    },
+  },
   filetypes = { 'go', 'gomod', 'gowork', 'gotmpl' },
   root_dir = function(bufnr, on_dir)
     local fname = vim.api.nvim_buf_get_name(bufnr)
